999a69fc29e61927a72b44c844d0bf666f736db7
[dyad] <diegovendedoralianca@gmail.com>
2025-12-06 16:22:25 -0300

    [dyad] Corrigindo erro de build TS6133 - wrote 1 file(s)

--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Product, Model } from '../types';
 import { Header } from '../components/Header';
 import { BottomNavigation } from '../components/BottomNavigation';

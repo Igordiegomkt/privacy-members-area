52ea5d45dbf265c9326ad40eff2811c617698969
[dyad] <diegovendedoralianca@gmail.com>
2025-12-23 19:10:36 -0300

    [dyad] Criei a tabela access_links com RLS e policies restritas a administradores. - wrote 2 file(s), executed 1 SQL queries

---
+++ supabase/migrations/0017_garantindo_a_fun_o_de_atualiza_o_de_timestamp_para_a_nova_tabela_.sql
@@ -0,0 +1,10 @@
+-- Function to update updated_at column (standard for this project)
+CREATE OR REPLACE FUNCTION public.update_updated_at_column()
+ RETURNS trigger
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$function$;
